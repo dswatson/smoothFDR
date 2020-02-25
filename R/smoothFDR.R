1bef336414fca2b5193c55381ce0709c60b7e5cc
dswatson <david.watson@oii.ox.ac.uk>
2020-02-25 14:57:07 +0000

    pkg

---
+++ R/smoothFDR.R
@@ -0,0 +1,126 @@
+#' FDR Smoothing
+#'
+#' This function implements Tansey et al.'s (2018) FDR smoothing algorithm.
+#'
+#' @param dat Data frame with columns for probe name, z-statistics, chromosomal 
+#'   position, and chromosome.
+#' @param probe String denoting column name for probes (CpGs, SNPs, etc.).
+#' @param z String denoting column name for z-scores.
+#' @param pos String denoting column name for chromosomal positions.
+#' @param chr String denoting column name for chromosomes.
+#' @param nulltype How should the null distribution be estimated? Choose 
+#'   \code{"empirical"} for Efron's central-matching method (default). Choose
+#'   \code{"theoretical"} for a standard normal null. 
+#' @param nlambda Length of the lambda sequence for the fused lasso subroutine.
+#' @param tol Convergence tolerance for the expectation maximization (EM) 
+#'   algorithm.
+#' @param maxit Maximum number of iterations for the EM algorithm.
+#' @param parallel Process in parallel? Only relevant if data spans multiple 
+#'   chromosomes. If \code{TRUE}, backend must be registered beforehand.
+#'
+#' @details
+#' FDR smoothing is an empirical Bayes method for exploiting spatial structure 
+#' in large multiple-testing problems. The method automatically finds 
+#' spatially localized regions of significant test statistics. It then relaxes 
+#' the threshold of statistical significance within these regions and tightens 
+#' it elsewhere, in a manner that controls the overall false discovery rate at a 
+#' given level. This results in increased power and cleaner spatial separation 
+#' of signals from noise. The approach requires solving a nonstandard 
+#' high-dimensional optimization problem, for which an efficient 
+#' augmented-Lagrangian algorithm is implemented. See (Tansey et al., 2018) 
+#' for details.
+#'
+#' @references
+#' Efron, B. (2004). \href{https://bit.ly/2kYx5AR}{Large-Scale Simultaneous 
+#' Hypothesis Testing: The Choice of a Null Hypothesis}. \emph{JASA, 99}(465),
+#' 96-104.
+#' 
+#' Newton, M.A. (2002). \href{https://bit.ly/2kYjCce}{On a Nonparametric 
+#' Recursive Estimator of the Mixing Distribution}. \emph{Sankhyā: The Indian
+#' Journal of Statistics, Series A, 64}(2), 306-322. 
+#' 
+#' Tansey, W., Koyejo, O., Poldrack, R.A., & Scott, J.G. (2018). 
+#' \href{https://bit.ly/2msEdWH}{False Discovery Rate Smoothing}. \emph{JASA, 
+#' 113}(523), 1156-1171.
+#' 
+#' Tibshirani, R., Saunders, M., Rosset, S., Zhu, J., & Knight, K. (2005). 
+#' \href{https://stanford.io/2lYzEmJ}{Sparsity and Smoothness via the Fused
+#' Lasso}. \emph{J. R. Statist. Soc. B, 67}(1), 91-108.
+#' 
+#' @examples
+#' # Import data
+#' data('DNAm_chr1')
+#' 
+#' # Set seed
+#' set.seed(123)
+#' 
+#' # Run FDR smoothing
+#' res <- smoothFDR(DNAm_chr1)
+#' 
+#' # Compare q-values to Benjamini-Hochberg estimates
+#' sum(res$BH <= 0.05)
+#' sum(res$q.value <= 0.05)
+#' 
+#' @export
+#' @importFrom data.table data.table
+#' @import foreach
+#' @import dplyr
+#' 
+
+
+smoothFDR <- function(dat, 
+                      probe = 'probe', 
+                      z = 'z', 
+                      pos = 'pos', 
+                      chr = 'chr', 
+                      nulltype = 'empirical', 
+                      nlambda = 30, 
+                      tol = 1e-6, 
+                      maxit = 100, 
+                      parallel = TRUE) {
+  # Check for errors, etc.
+  if (!is.data.frame(dat)) {
+    stop('dat must be a data.frame.')
+  }
+  if (!probe %in% colnames(dat)) {
+    stop('Column \"', probe, '\" not found.')
+  }
+  if (!z %in% colnames(dat)) {
+    stop('Column \"', z, '\" not found.')
+  }
+  if (!pos %in% colnames(dat)) {
+    stop('Column \"', pos, '\" not found.')
+  }
+  if (!chr %in% colnames(dat)) {
+    stop('Column \"', chr, '\" not found.')
+  }
+  # Prepare data
+  dat <- data.table(
+      'Idx' = seq_len(nrow(dat)), 
+    'probe' = dat[[probe]], 
+        'z' = dat[[z]],
+      'pos' = dat[[pos]], 
+      'chr' = dat[[chr]]
+  )
+  # Execute in parallel or serial
+  if (parallel) {
+    out <- foreach(ch = dat[, unique(chr)], .combine = rbind) %dopar%
+      smooth_fdr(probe = dat[chr == ch, probe], z = dat[chr == ch, z], 
+                 pos = dat[chr == ch, pos], chr = ch, nulltype = nulltype,
+                 nlambda = nlambda, tol = tol, maxit = maxit)
+  } else {
+    out <- foreach(ch = dat[, unique(chr)], .combine = rbind) %do%
+      smooth_fdr(probe = dat[chr == ch, probe], z = dat[chr == ch, z], 
+                 pos = dat[chr == ch, pos], chr = ch, nulltype = nulltype,
+                 nlambda = nlambda, tol = tol, maxit = maxit)
+  }
+  # Rearrange and export
+  dat %>% 
+    select(Idx, probe) %>%
+    inner_join(out, by = 'probe') %>% 
+    arrange(Idx) %>%
+    select(-Idx) %>%
+    return(.)
+}
+
+
---
+++ R/utils.R
@@ -0,0 +1,87 @@
+#' Smooth FDR workhorse
+#' 
+#' This is the workhorse of the smoothFDR package. The eponymous function
+#' is just a wrapper.
+#'
+#' @import FDRreg
+#' @import glmgen
+#' @import dplyr
+#' 
+
+smooth_fdr <- function(probe, z, pos, chr, nulltype, nlambda, tol, maxit) {
+  ### Part I: Estimate null and alternative distros
+  n <- length(z)
+  x <- pos - min(pos) + 1
+  fdr1 <- FDRreg(z, x, nulltype = nulltype)
+  f0 <- fdr1$M0
+  f1 <- fdr1$M1
+  p0 <- fdr1$p0
+  ### Part II: Expectation-Maximization
+  # Initial parameters for EM algorithm
+  drift <- 1
+  iter <- 1
+  prior_prob <- rep(1 - p0, n)
+  beta_hat <- qlogis(prior_prob)
+  m1 <- prior_prob * f1
+  m0 <- (1 - prior_prob) * f0
+  post_prob <- m1 / (m1 + m0)
+  wts <- prior_prob * (1 - prior_prob)
+  y <- beta_hat - (prior_prob - post_prob) / wts
+  fl0 <- trendfilter(y, weights = wts, k = 0, nlambda = nlambda)
+  obj_old <- sum(log(prior_prob * f1 + (1 - prior_prob) * f0))
+  # Data frame to keep track of BIC, matrix for pi vectors
+  res <- data.frame('lambda' = fl0$lambda, 'BIC' = NA_real_)
+  pi <- matrix(nrow = n, ncol = nlambda + 1)
+  pi[, 1] <- prior_prob
+  # EM algorithm
+  em <- function(lambda, prior_prob) {
+    idx <- which(res$lambda == lambda)
+    beta_hat <- qlogis(prior_prob)
+    while(drift >= tol && iter <= maxit) {
+      if (iter == 1) {
+        fl <- trendfilter(y, weights = wts, k = 0, lambda = lambda)
+      } else {
+        # E step
+        m1 <- prior_prob * f1
+        m0 <- (1 - prior_prob) * f0
+        post_prob <- m1 / (m1 + m0)
+        # M step
+        wts <- prior_prob * (1 - prior_prob)
+        y <- beta_hat - (prior_prob - post_prob) / wts
+        fl <- trendfilter(y, weights = wts, k = 0, lambda = lambda)
+      }
+      # Update  
+      beta_hat <- as.numeric(fl$beta)
+      prior_prob <- plogis(beta_hat)
+      obj_new <- sum(log(prior_prob * f1 + (1 - prior_prob) * f0))
+      drift <- abs(obj_old - obj_new) / (abs(obj_old) + tol)
+      iter <- iter + 1
+      obj_old <- obj_new
+    }
+    out <- list('BIC' = -2 * obj_new + log(n) * fl$df, 'pi1' = prior_prob)
+    return(out)
+  }
+  # Loop over lambdas using previous outputs as warm starts
+  for (i in seq_len(nlambda)) {
+    tmp <- em(res$lambda[i], pi[, i])
+    res$BIC[i] <- tmp$BIC
+    pi[, (i + 1)] <- tmp$pi1
+  }
+  pi <- pi[, -1]
+  ### Part III: Use posteriors to compute FDR
+  j <- which.min(res$BIC)
+  m1 <- pi[, j] * f1
+  m0 <- (1 - pi[, j]) * f0
+  post_prob <- m1 / (m1 + m0)
+  # Export data frame
+  data.frame(Idx = seq_len(n), probe = probe, z = z, p.value = pnorm(z), 
+             pos = pos, chr = chr) %>%
+    mutate(lfdr = 1 - post_prob) %>%
+    arrange(lfdr) %>%
+    mutate(q.value = cummean(lfdr)) %>%
+    arrange(Idx) %>%
+    select(-Idx) %>%
+    return(.)
+}
+
+
